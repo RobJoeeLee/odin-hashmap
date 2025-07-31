f39d7dc11cdcd21d99dce801c0948e5285441fd8
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-31 14:22:27 -0400

    created the remove method

--- hash.js
+++ hash.js
@@ -92,4 +92,25 @@ class HashMap {
 
     return false;
   }
+
+  remove(key) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, _] = bucket[i];
+      if (storedKey === key) {
+        bucket.splice(i, 1);
+        this.size--;
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
