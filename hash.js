2c6aa4bfeca783dce3f52dcc33b85a2666b0360d
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-31 14:00:54 -0400

    created the hash method (be able to hash through keys)

--- hash.js
+++ hash.js
@@ -5,4 +5,15 @@ class HashMap {
     this.buckets = new Array(this.capacity).fill(null).map(() => []);
     this.size = 0;
   }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (hashCode * primeNumber + key.charCodeAt[i]) % this.capacity;
+    }
+
+    return hashCode;
+  }
 }
