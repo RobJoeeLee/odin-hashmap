6f04a4f459fec59962f4e62fc069cb34b9fa130e
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-31 13:58:16 -0400

    started the HashMap class and created the contructor

--- hash.js
+++ hash.js
@@ -1,1 +1,8 @@
-console.log("I am Hash");
+class HashMap {
+  constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.capacity = initialCapacity;
+    this.loadFactor = loadFactor;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.size = 0;
+  }
+}
